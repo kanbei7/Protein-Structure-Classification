071cfdc66b7af2226fda5140cb52676ef12ee516
kanbei7 <ericliu_007@sina.com>
2019-05-03 17:56:28 -0500

    model0

--- model_clf.py
+++ model_clf.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BiDirLSTM(nn.Module):
-	def __init__(self, vocab_size, embedding_dim, n_classes,rnn_hidden_size, n_layers = 1, pbatch_size=1):
+	def __init__(self, vocab_size, embedding_dim, n_classes,rnn_hidden_size, batch_size, n_layers = 1, pbatch_size=1):
 		super(BiDirLSTM, self).__init__()
 		self.vocab_size = vocab_size
 		self.embedding_dim = embedding_dim
@@ -11,15 +11,16 @@ class BiDirLSTM(nn.Module):
 		self.rnn_hidden_size = rnn_hidden_size
 		self.n_layers = n_layers
 		self.pbatch_size = pbatch_size
+		self.batch_size = batch_size
 
 		self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
 
 		#TB
 		self.rnn_input_size = self.embedding_dim*self.pbatch_size
-		self.rnn = nn.LSTM(self.rnn_input_size, self.rnn_hidden_size, batch_first = True, bidirectional = True, num_layers = self.n_layers)
+		self.rnn = nn.LSTM(self.rnn_input_size, self.rnn_hidden_size, bidirectional = True, num_layers = self.n_layers)
 
 		self.main_clf = nn.Sequential(
-			nn.Linear(rnn_hidden_size, int(rnn_hidden_size/4)),
+			nn.Linear(2*rnn_hidden_size, int(rnn_hidden_size/4)),
 			nn.ReLU(),
 			nn.Linear(int(rnn_hidden_size/4),int(rnn_hidden_size/4)),
 			nn.Linear(int(rnn_hidden_size/4), n_classes)
@@ -29,10 +30,10 @@ class BiDirLSTM(nn.Module):
 	def forward(self, seq):
 		#batch_size, seq_length, embed_dim
 		embeds = self.word_embeddings(seq)
-		_, (rnn_out , _) = self.rnn(embeds)
-		pred_scores = main_clf(rnn_out.squeeze())
+		_, (rnn_out , _) = self.rnn(embeds.transpose(0,1))
+		pred_scores = self.main_clf(torch.cat([rnn_out[0], rnn_out[1]] , dim = 1) )
 		return pred_scores 
 
-	def load_embeddings(pre_embd):
+	def load_embeddings(self, pre_embd):
 		self.word_embeddings = nn.Embedding.from_pretrained(pre_embd)
 
